685c13374cca6332232f9169593f0f29d86b9ba1
Dominique Miller <dominique.p.miller@gmail.com>
2016-01-03 11:18:14 -0500

    added bpopup and more project info in slides

--- js/index.js
+++ js/index.js
@@ -38,3 +38,23 @@ $(document).ready(function() {
   });
 
 });
+
+$(document).on('ready', function(){
+  $('.projects').click(function(){
+    $('.ruby-projects').bPopup({
+      speed: 650,
+      transition: 'slideIn',
+      transitionClose: 'slideBack'
+    });
+  });
+  $('.projects-ii').click(function(){
+    $('.rails-projects').bPopup({
+      speed: 650,
+      transition: 'slideIn',
+      transitionClose: 'slideBack'
+    });
+  });
+  $('.contact-info-top').click(function(){
+    $('.contact-info').bPopup();
+  });
+});
